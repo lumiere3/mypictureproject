6bad5d4735b164f7b4a5a429602073a9062f802e
Lumine <1948906985@qq.com>
2025-10-29 20:52:20 +0800

    初始化

---
+++ src/main/java/com/lumine3/luminapicturebackend/LuminaPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lumine3.luminapicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminaPictureBackendApplication.class, args);
+    }
+
+}
